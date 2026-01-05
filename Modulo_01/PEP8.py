cd36633f4d52c88f06392bf5dc6b60fb0126880d
Paulo Carvalho <paulo.carvalho@global.ntt>
2026-01-05 16:55:31 -0300

    aprendendo sobre guias de estilos

--- Modulo_01/PEP8.py
+++ Modulo_01/PEP8.py
@@ -2,3 +2,28 @@
 # https://peps.python.org/pep-0008/
 # Propostas de melhorias para a linguagem Python
 
+"""
+
+[1] - utilize Camel case para nomes de classes;
+
+class Calculadora:
+    pass
+
+class CalculadoraNumerica:
+    pass
+
+[2] - Utilize nomes em minusculos, separados para underline para funções ou variaveis
+    
+def soma():
+    pass
+
+def soma_dois():
+    pass
+
+numero = 4
+numero_impar = 5
+
+[3] - Utilize 4 espaços para identação!
+
+
+"""
