6a7edf57e0baae57dd7c141e4ae06c7ce64cd33a
Paulo Carvalho <paulo.carvalho@global.ntt>
2026-01-05 17:38:32 -0300

    feat: entendendo sobre help e dir como utilIzar

--- Modulo_01/aula03_dir_help.py
+++ Modulo_01/aula03_dir_help.py
@@ -3,7 +3,7 @@ Utilitarios Python para auxiliar na programação
 
 dir => Apresenta todos os atributos e funções/métodos disponíveis para determinado tipo de dado ou variavel
 
-Help => Apresenta a documentação/como utilizar os atribuitos/propriedades e funções métodos disponiveis  
+help => Apresenta a documentação/como utilizar os atribuitos/propriedades e funções métodos disponiveis  
 para determinado tipo de dados ou variavel
 
 """
